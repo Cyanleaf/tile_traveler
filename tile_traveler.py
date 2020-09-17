3b088fa39fe87e770ad57ed04ad6bc5ba2458bd1
Kristjan O. Ragnarsson <kristjan@ragnarsson.im>
2020-09-17 09:56:04 +0000

    main structure w/o func  done

--- tile_traveler.py
+++ tile_traveler.py
@@ -2,3 +2,24 @@
 https://github.com/Kristjan-O-Ragnarsson/tile_traveler
 tile taveler a til movement program move betvine tiles in a 3x3 env
 """
+
+
+
+posision_1_1 = True
+posision_1_2 = False
+posision_1_3 = False
+posision_2_1 = False
+posision_2_2 = False
+posision_2_3 = False
+posision_3_1 = False
+posision_3_2 = False
+posision_3_3 = False
+
+kill = False
+
+while not kill:
+    user_input = input("Direction: ")
+    
+    if posision_3_1:
+        print("Victory!")
+        kill = True
