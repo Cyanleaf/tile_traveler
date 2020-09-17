524d0708bf6b0a208ca0081139b740696c9251ff
Kristjan O. Ragnarsson <kristjan@ragnarsson.im>
2020-09-17 10:18:22 +0000

    added basic functionality for valid_directions

--- tile_traveler.py
+++ tile_traveler.py
@@ -3,17 +3,40 @@ https://github.com/Kristjan-O-Ragnarsson/tile_traveler
 tile taveler a til movement program move betvine tiles in a 3x3 env
 """
 
+def valid_directions():
+    """
+    returns valid directions for player in string format
+    """
+    valid = "nesw"
+    if x == 1:
+        valid.replace("w", "")
+    if y == 1:
+        valid.replace("s", "")
+    if x == 3:
+        valid.replace("e", "")
+    if y == 3:
+        valid.replace("n", "")
+    
+    if x == 1 and y == 1:
+        valid.replace("e", "")
+    elif x == 2 and y == 1:
+        valid.replace("e", "")
+        valid.replace("w", "")
+    elif x == 2 and y == 2:
+        valid.replace("n", "")
+        valid.replace("e", "")
+    elif x == 2 and y == 3:
+        valid.replace("s", "")
+    elif x == 3 and y == 2:
+        valid.replace("w", "")
 
+    return valid
 
-posision_1_1 = True
-posision_1_2 = False
-posision_1_3 = False
-posision_2_1 = False
-posision_2_2 = False
-posision_2_3 = False
-posision_3_1 = False
-posision_3_2 = False
-posision_3_3 = False
+def move_player(user_input):
+    pass
+
+x = 1
+y = 1
 
 kill = False
 
