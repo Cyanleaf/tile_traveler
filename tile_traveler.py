dd5a5727c8aeffa335052cb3f7472f9f329413ac
Kristjan O. Ragnarsson <kristjan@ragnarsson.im>
2020-09-17 11:03:51 +0000

    fixed valid_travel

--- tile_traveler.py
+++ tile_traveler.py
@@ -3,40 +3,57 @@ https://github.com/Kristjan-O-Ragnarsson/tile_traveler
 tile taveler a til movement program move betvine tiles in a 3x3 env
 """
 
-def valid_directions():
+def valid_directions(x, y):
     """
     returns valid directions for player in string format
     """
+    print(x == 1, y)
     valid = "nesw"
     if x == 1:
-        valid.replace("w", "")
+        valid = valid.replace("w", "")
     if y == 1:
-        valid.replace("s", "")
+        valid = valid.replace("s", "")
     if x == 3:
-        valid.replace("e", "")
+        valid = valid.replace("e", "")
     if y == 3:
-        valid.replace("n", "")
-    
+        valid = valid.replace("n", "")
+    print(valid)
     if x == 1 and y == 1:
-        valid.replace("e", "")
+        valid = valid.replace("e", "")
     elif x == 2 and y == 1:
-        valid.replace("e", "")
-        valid.replace("w", "")
+        valid = valid.replace("e", "")
+        valid = valid.replace("w", "")
     elif x == 2 and y == 2:
-        valid.replace("n", "")
-        valid.replace("e", "")
+        valid = valid.replace("n", "")
+        valid = valid.replace("e", "")
     elif x == 2 and y == 3:
-        valid.replace("s", "")
+        valid = valid.replace("s", "")
     elif x == 3 and y == 2:
-        valid.replace("w", "")
+        valid = valid.replace("w", "")
 
     return valid
 
-def valid_travel_print():
-    
+def valid_travel_print(x, y):
+    valid = valid_directions(x, y)
+    print(valid)
+    print("You can travel: ", end="")
+    to_print = ""
+    for i in valid:
+        if i == valid[-1] and i != valid[0]:
+            to_print += " or"
+        if i == "n":
+            to_print += " (N)orth "
+        elif i == "e":
+            to_print += " (E)ast "
+        elif i == "s":
+            to_print += " (S)outh "
+        elif i == "w":
+            to_print += " (W)est "
+        
+    print(to_print + ".")
 
 
-def move_player(user_input):
+def move_player(user_input, x, y):
     valid = valid_directions()
     user_input_clean = user_input.strip().lower()
     if user_input_clean in valid:
@@ -58,8 +75,9 @@ y = 1
 kill = False
 
 while not kill:
+    valid_travel_print(x, y)
     user_input = input("Direction: ")
     
-    if posision_3_1:
+    if x == 3 and y == 1:
         print("Victory!")
         kill = True
